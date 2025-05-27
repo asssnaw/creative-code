c7be87cea5f650982b9056f10be1b4b1a18c8700
asssnaw <asnafatima2006@gmail.com>
2025-05-27 10:37:23 +0400

    poisterize.html

---
+++ Activity4 - Poisterize effect/sketch.js
@@ -0,0 +1,30 @@
+var img;
+
+function preload() {
+
+img = loadImage("scene.jpg");
+
+}
+
+
+
+function setup () {
+
+createCanvas (600, 400);
+
+background(0);
+
+}
+
+
+
+function draw() {
+
+background(0);
+
+image(img, 0, 0);  
+
+var v = map(mouseX, 0, width, 2, 20);
+
+filter(POSTERIZE, v); 
+}
